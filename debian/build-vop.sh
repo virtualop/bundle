4f47df9ab2673ca67b1cd2d52f1b160717818892
Philipp T <philipp@hitchhackers.net>
2023-09-03 18:31:40 +0300

    snafoo

--- debian/build-vop.sh
+++ debian/build-vop.sh
@@ -9,7 +9,7 @@ VOP_TARGET=$DEBIAN_DIR/vop/usr/lib/vop
 echo "vop root : $VOP_ROOT"
 echo "target dir : $VOP_TARGET"
 
-mkdir -p $VOP_TARGET
+rm -rf $VOP_TARGET && mkdir -p $VOP_TARGET
 cd $VOP_ROOT
 cp -r bin exe Gemfile* lib Rakefile vendor *.gemspec $VOP_TARGET
 
--- dev/vop.sh
+++ dev/vop.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# TODO generalize
+eval "$(rbenv init - bash)"
+
 BUNDLE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
 
 cd $BUNDLE_DIR
